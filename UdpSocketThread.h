8e4503aa8830a86e09d11ad03f04a4c053a6f43e
wavem <fenix24@naver.com>
2020-08-06 16:14:23 +0900

    [New] UDP Socket Thread (Step 02)
    - Packet Size 관련 define 정의.
    - UDP Socket Thread Class 선언.

--- UdpSocketThread.h
+++ UdpSocketThread.h
@@ -3,4 +3,40 @@
 #ifndef UdpSocketThreadH
 #define UdpSocketThreadH
 //---------------------------------------------------------------------------
+#include "Main.h"
+
+enum ThreadWorkingType
+{
+	THREAD_STOP = 0,
+	THREAD_RUNNING = 1,
+	THREAD_TERMINATED = 2,
+};
+//---------------------------------------------------------------------------
+
+class CUdpSocketThread : public TThread {
+private:
+	ThreadWorkingType	m_eThreadWork;
+	SOCKET	*m_sock;
+
+protected:
+public: // Variables
+	BYTE sendBuff[UDP_SEND_BUF_SIZE];
+	BYTE* p_sendText;
+	BYTE recvBuff[UDP_RECV_BUF_SIZE];
+	//SERVERDATA recvData;
+	bool isTryingToConnect;
+	bool isConnected;
+
+public: // Functions
+	__fastcall CUdpSocketThread(SOCKET *p_sock);
+	__fastcall ~CUdpSocketThread();
+	bool __fastcall Receive();
+	void __fastcall Execute();
+	void __fastcall Stop();
+	void __fastcall Resume();
+	void __fastcall DoTerminate();
+	ThreadWorkingType __fastcall GetThreadStatus();
+};
+
+//---------------------------------------------------------------------------
 #endif
