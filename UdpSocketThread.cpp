56ff2d93e0509ff385b4664f19eb98fce8799d81
wavem <fenix24@naver.com>
2020-08-07 16:17:16 +0900

    [New] UDP Socket Thread (Step 03)
    - Thread 기본 함수 정의. (stop, resume 등등)

--- UdpSocketThread.cpp
+++ UdpSocketThread.cpp
@@ -5,3 +5,32 @@
 #include "UdpSocketThread.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+
+
+
+
+
+
+
+
+
+
+void __fastcall CUdpSocketThread::Stop() {
+	m_eThreadWork = THREAD_STOP;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall CUdpSocketThread::Resume() {
+	m_eThreadWork = THREAD_RUNNING;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall CUdpSocketThread::DoTerminate() {
+	m_eThreadWork = THREAD_TERMINATED;
+}
+//---------------------------------------------------------------------------
+
+ThreadWorkingType __fastcall CUdpSocketThread::GetThreadStatus() {
+	return m_eThreadWork;
+}
+//---------------------------------------------------------------------------
