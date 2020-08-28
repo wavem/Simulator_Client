76c9269033a76208c0fb22c22dc78bf029f970c4
wavem <fenix24@naver.com>
2020-08-28 10:42:41 +0900

    [Mod] IP Address
    - HM -> Globiz

--- Define.h
+++ Define.h
@@ -6,7 +6,8 @@
 
 
 #define UDP_SERVER_PORT	16000
-#define IP_SERVER "118.223.164.124"
+//#define IP_SERVER "118.223.164.124" // HM
+#define IP_SERVER "112.217.45.202" // Globiz
 #define IP_LOCAL "192.168.0.47"
 
 #define UDP_SEND_BUF_SIZE 256
