72e57970eb986a7a91489a760621bc2c88dbbb1b
wavem <fenix24@naver.com>
2020-09-16 11:26:49 +0900

    [New] Bit Control Funcs

--- common_functions.h
+++ common_functions.h
@@ -4,4 +4,13 @@
 #define common_functionsH
 //---------------------------------------------------------------------------
 
+#include <Windows.h>
+
+
+
+extern bool _BitCheck(BYTE _src, int _bit);
+extern BYTE _BitSetting(BYTE _src, int _bitIdx, bool _bool);
+
+
+//---------------------------------------------------------------------------
 #endif
