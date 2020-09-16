619e4e1e67bbc478b96cab8c7b5a15708c17702c
wavem <fenix24@naver.com>
2020-09-16 11:27:43 +0900

    [Add] Include common_functions

--- Main.cpp
+++ Main.cpp
@@ -6,6 +6,7 @@
 #include "Main.h"
 #include "Dlg_Version.h"
 #include "libxl_functions.h"
+#include "common_functions.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma link "cxClasses"
