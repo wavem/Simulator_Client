0dabb2eebc2140b0c7d21280fa7982c4854fe1e6
wavem <fenix24@naver.com>
2020-09-24 11:16:46 +0900

    [Mod] Data Input Dlg Parameter
    - Byte Size 와 Bit Size 를 Main 에서 모두 계산해서 넘겨주도록 하자.
    - Data Input Dlg 에서는 말 그대로 전달받은 정확한 정보만을 토대로 해서 오직 계산에만 집중하도록 한다.

--- Main.cpp
+++ Main.cpp
@@ -1159,7 +1159,7 @@ void __fastcall TFormMain::OnDbClickCell_Protocol(TObject *Sender, int ARow, int
 
 
 	// Create Data Input Dialog
-	TFormDataInputEdit* p_dlg = new TFormDataInputEdit(t_pBuffer, t_Row - 1, t_Span_Y, 8 - t_Col, t_Span_X);
+	TFormDataInputEdit* p_dlg = new TFormDataInputEdit(t_pBuffer, t_Row - 1, t_Span_Y + 1, 8 - t_Col, t_Span_X + 1);
 	p_dlg->ShowModal();
 	delete p_dlg;
 
