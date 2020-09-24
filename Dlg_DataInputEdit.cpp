b12d05c9f043dd2b65d90f68d260ab199f1d9098
wavem <fenix24@naver.com>
2020-09-24 11:54:25 +0900

    [Add] Data Input Dlg : 7 Bit Processing
    - Test Complete.
    - 이 상태로 1바이트 짜리는 쭉 작업해 나가면 될 듯.

--- Dlg_DataInputEdit.cpp
+++ Dlg_DataInputEdit.cpp
@@ -26,17 +26,32 @@ __fastcall TFormDataInputEdit::TFormDataInputEdit(BYTE* _pBuffer, int _ByteIdx, 
 	m_BitIdx = _BitIdx;
 	m_ByteSize = _ByteSize;
 	m_BitSize = _BitSize;
-
 	int t_CurrentValue = 0;
 
+	// Extract Current Value Routine
 	switch(m_ByteSize) {
 		default:
 			break;
 
 		case 1:
 		{
-			if(m_BitSize == 8) {
-				t_CurrentValue = m_pBuffer[m_ByteIdx];
+			switch(m_BitSize) {
+				case 8:
+					t_CurrentValue = m_pBuffer[m_ByteIdx];
+					break;
+				case 7:
+					t_CurrentValue = m_pBuffer[m_ByteIdx] >> (_BitIdx - 6) & 0x7F;
+					break;
+				case 6:
+					break;
+				case 5:
+					break;
+				case 4:
+					break;
+				case 3:
+					break;
+				case 2:
+					break;
 			}
 			break;
 		}
